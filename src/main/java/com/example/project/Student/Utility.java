ea9d87456969e0212724de4763ad63338bcfd6aa
Joshua <139407249+joshf80@users.noreply.github.com>
2025-02-15 05:59:46 +0000

    finish

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -5,6 +5,34 @@ import java.util.Arrays;
 public class Utility {
 
     public static ArrayList<Student> sortStudents(ArrayList<Student> list){
+        int n = list.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                Student s1 = list.get(j);
+                Student s2 = list.get(j + 1);
+                if (s1.getLastName().compareTo(s2.getLastName()) > 0) {
+                    Student temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+                if (s1.getLastName().compareTo(s2.getLastName()) == 0 && s1.getFirstName().compareTo(s2.getFirstName()) > 0) {
+                    Student temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+                if (s1.getLastName().compareTo(s2.getLastName()) == 0 && s1.getFirstName().compareTo(s2.getFirstName()) == 0 && s1.getGpa() < s2.getGpa()) { 
+                    Student temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+
+
+            }
+
+        }
+
         return list;
     }
 
